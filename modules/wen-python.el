692d2578fd3f078ea7bbf8f36eb0ec8341e35308
jouyouyun <jouyouwen717@gmail.com>
2016-09-13 20:15:07 +0800

    module: Using elpy && jedi for python

--- modules/wen-python.el
+++ modules/wen-python.el
@@ -2,26 +2,41 @@
 
 (wen-require-packages '(
                         python-mode
-                        anaconda-mode
-                        pythonic
-                        company-anaconda
+                        pyvenv
+                        elpy
+                        jedi-core
+                        company-jedi
                         ))
 
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i")
 
-;; company-anaconda: complete for python
-;; depends: anaconda, pythonic
-(require 'pythonic)
-(require 'anaconda-mode)
-(require 'company-anaconda)
+;; elpy
+;; depends: jedi, flake8, importmagic, autopep8, yapf
+;; query config 'M-x elpy-config'
+;; pyvenv: active virtualenv 'M-x pyvenv-activate'
+;; deactive 'M-x pyvenv-deactivate'
+(defun wen-elpy-setup()
+  (elpy-enable)
+  (elpy-use-ipython)
+  (setq elpy-rpc-backend "jedi")
+  (setq elpy-rpc-python-command "python")
+  (when (require 'flycheck nil t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
+  )
 
 (eval-after-load "python"
   `(progn
-     (add-hook 'python-mode-hook 'anaconda-mode)
+     (wen-elpy-setup)
      (add-hook 'python-mode-hook 'eldoc-mode)
-     (add-to-list 'company-backends 'company-anaconda)
+     ;; Standard Jedi.el setting
+     (add-hook 'python-mode-hook 'jedi:setup)
+     (setq jedi:complete-on-dot t)
+     (add-to-list 'company-backends 'company-jedi)
      ))
 
 (provide 'wen-python)
